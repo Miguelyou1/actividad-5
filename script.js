9c72a266118cb8a87a6063062a91c52f0edc0a6c
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-04-19 19:43:08 +0200

    script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var nombre = prompt("introduzca su nombre");
 nombre = nombre.toLowerCase().replace(/\s+/g, '');
 var nombreAlReves = '';
+for (var i = nombre.length - 1; i >= 0; i--) {
+    nombreAlReves += nombre[i];
+}
