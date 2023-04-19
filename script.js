b850cf853bb40a64847b9334d217c1ca20298691
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-04-19 19:40:34 +0200

    script

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let nombreDeLaVariable = 'nombre de ususario';
-alert('¡Hola!');
-nombreDeLaVariable = prompt("introduzca un nombre");
-alert('Hola ' +nombreDeLaVariable)
-int i
-for(i=n;i<-1;i--)
+var nombre = prompt("introduzca su nombre");
